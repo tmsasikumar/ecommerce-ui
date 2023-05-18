004b55d928b5e3b840520fb01b43d045e020e75a
Mohamed Idris A <mohamedidris.a@thoughtworks.com>
2023-05-18 18:39:25 +0530

    Style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,16 @@ const Header = () => {
             <Nav />
             <div className="home__banner">
                 <div className="home__bannerInfo">
-                    <h1>Shop Now</h1>
-                    <p>Shop for the latest fashion trends</p>
+                    <div className="home__title">Shop Now</div>
+                    <div className="home__message">Shop for the latest fashion trends</div>
                     <Link to="/products">
                         <button className="home__bannerButton">Shop Now</button>
                     </Link>
+                </div>
+
+                <div className="home__offers">
+                    <div className="home__offer">FLAT 50%</div>
+                    <div className="home__offer">BUY 1, GET 2</div>
                 </div>
             </div>
         </Fragment>
